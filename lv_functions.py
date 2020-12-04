0918aae79582173bfe4e79d1e91c259d9ad3b8dd
lashhw <lashhw81@gmail.com>
2020-12-04 14:21:48 +0800

    create interface for LabView to multiprocess

--- lv_functions.py
+++ lv_functions.py
@@ -51,22 +51,32 @@ def get_optimal_move(data_tuple):
     tmp_board = bd.Board(np.array(data_tuple[0], dtype="uint8"))
     return (tmp_board.get_data().tolist(), ai.get_optimal_move(tmp_board), 0, 0, "")
 
-def get_strong_optimal_move(data_tuple):
-    board = 0
-    i = 0
-    for row in data_tuple[0]:
-        for c in row:
-            board |= int(c) << (4*i)
-            i += 1
-    os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/strong-ai/bin")
-    output = subprocess.Popen(["2048.exe", str(board)], stdout=subprocess.PIPE).communicate()[0]
-    return (data_tuple[0], int(str(output)[-2:-1]), 0, 0, "")
-
 def get_move_expected_score(data_tuple):
     tmp_board = bd.Board(np.array(data_tuple[0], dtype="uint8"))
-    return (tmp_board.get_data().tolist(), ai.get_move_expected_score(tmp_board, data_tuple[1], data_tuple[2], data_tuple[3]), 0, 0, "")
+    return ai.get_move_expected_score(tmp_board, data_tuple[1], data_tuple[2], data_tuple[3])
+
+def get_strong_optimal_move(data_tuple):
+    board = __to_c_board(data_tuple[0])
+    os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/strong-ai/bin")
+    output = subprocess.Popen(["2048.exe", str(board)], stdout=subprocess.PIPE).communicate()[0]
+    return (data_tuple[0], int(output.decode("utf-8")), 0, 0, "")
+
+def get_strong_move_expected_score(data_tuple):
+    board = __to_c_board(data_tuple[0])
+    os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/strong-ai/bin")
+    output = subprocess.Popen(["2048_get_score.exe", str(board), str(data_tuple[1])], stdout=subprocess.PIPE).communicate()[0]
+    return float(output.decode("utf-8"))
 
 def get_readable_board(data_tuple):
     converted_np = 2 ** np.array(data_tuple[0], dtype="uint")
     converted_np[converted_np == 1] = 0
     return (converted_np.tolist(), 0, 0, 0, "")
+
+def __to_c_board(board_list):
+    board = 0
+    i = 0
+    for row in board_list:
+        for c in row:
+            board |= int(c) << (4*i)
+            i += 1
+    return board
--- strong-ai/build.bat
+++ strong-ai/build.bat
@@ -1,4 +1,5 @@
 mkdir bin
 cd bin
 g++ ../2048.cpp -O3 -o 2048.exe
+g++ ../2048_get_score.cpp -O3 -o 2048_get_score.exe
 pause
